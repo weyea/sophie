0d93204c26c395fd79d22ebe03e5a5c75346d5a5
伯飞 <bofei.zq@taobao.com>
2016-10-15 00:35:04 +0800

    update

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -65,7 +65,7 @@
       var walk = function(vnode){
 
          var currentData = {};
-         var children=vnode.children || vnode.attributes&&vnode.attributes.children;
+         var children=vnode.children&&vnode.children.length || vnode.attributes&&vnode.attributes.children;
 
         if(Sophie.isThunk(vnode)){
           var component = vnode;

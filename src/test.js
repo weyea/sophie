bbc80094840d7e37975e16c7db29222e9a8168db
伯飞 <bofei.zq@taobao.com>
2017-08-30 15:25:09 +0800

    udpate

--- src/test.js
+++ src/test.js
@@ -1,96 +1,4 @@
 
-  Sophie.createStyleSheet({
-    "my-button":{
-      border:"solid 1px red",
-      display:"block"
-    }
-  })
-
-
-var WhatIWantToSay = "header"
-
-var  MyDivHeader = Sophie.createClass("my-div-header", {
-
-  componentWillMount:function(){
-
-
-  },
-
-  componentDidMount:function(){
-    console.log("header")
-  },
-
-  render:function(){
-    return (<div>header</div>)
-  }
-
-})
-
-
-var  MyDivBody = Sophie.createClass("my-div-body", {
-
-  componentWillMount:function(){
-
-
-  },
-
-  componentDidMount:function(){
-    console.log("body")
-  },
-
-  render:function(){
-    return (<div>body</div>)
-  }
-
-})
-
-
-
-var MyHTMLDiv = Sophie.createClass("my-div",{
-  name:"BaseDiv",
-  type:"span",
-  num:0,
-
-  componentWillMount:function(){
-    this.state.name=123
-  },
-  componentDidMount:function(){
-
-    this.setState({name:123})
-    console.log("gogog")
-
-    setInterval(()=>{
-      this.num++;
-    this.setState({name:[this.num,123]})
-
-    },1000)
-  },
-
-  render: function  () {
-
-    return (
-       this.children
-    )
-  }
-
-})
-
-
-
-  var BaseDiv = Sophie.createClass("base-div",{
-
-
-    render: function() {
-
-      return (
-        <div class="baseDiv" data-name={this.name}>
-            {this.props.children}
-        </div>
-      )
-    }
-  })
-
-
 
 
     var MyJSDiv = Sophie.createClass("my-js-div",{
@@ -115,7 +23,7 @@ var MyHTMLDiv = Sophie.createClass("my-div",{
         return (
           <div class="container">
               <div class="123">
-                <BaseDiv ref="baseDiv">basediv{this.state.name}</BaseDiv>
+                <svg class="grid-lines"><line x1="150" y1="0" x2="150" y2="100%" class="backLine"></line><line x1="1130" y1="0" x2="1130" y2="100%" class="backLine"></line><line x1="0" y1="128" x2="100%" y2="128" class="backLine"></line><line x1="0" y1="5817" x2="100%" y2="5817" class="backLine"></line><line x1="150" y1="0" x2="150" y2="100%" class="frontLine"></line><line x1="1130" y1="0" x2="1130" y2="100%" class="frontLine"></line><line x1="0" y1="128" x2="100%" y2="128" class="frontLine"> </line><line x1="0" y1="5817" x2="100%" y2="5817" class="frontLine"></line></svg>
               </div>
 
           </div>
@@ -135,4 +43,4 @@ Sophie.on("ready", function(){
 
 
  Sophie.runApp(MyJSDiv)
- Sophie.createVnodeByTagName("my-js-div")
+

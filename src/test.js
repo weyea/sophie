dbf76bdc0aae95b3fed4d3f53f3d44eb144c370c
伯飞 <bofei.zq@taobao.com>
2017-09-05 16:34:24 +0800

    udpate

--- src/test.js
+++ src/test.js
@@ -1,3 +1,4 @@
+
 var Div1 = Sophie.createClass("my-js-div-1",{
     getDefaultProps:function(){
       return {
@@ -15,11 +16,13 @@ var Div1 = Sophie.createClass("my-js-div-1",{
 
 
 var Div2 = Sophie.createClass("my-js-div-2",{
+
     getDefaultProps:function(){
         return {
             id:"div-2"
         }
     },
+
     render: function  () {
         return (
             <div id={this.props.id} class="div-2">
@@ -32,7 +35,6 @@ var Div2 = Sophie.createClass("my-js-div-2",{
 
 var Div3 = Sophie.createClass("my-js-div-3",{
     getDefaultProps:function(){
-
         return {
             id:"div-3"
         }
@@ -55,19 +57,21 @@ var Div3 = Sophie.createClass("my-js-div-3",{
 
 var MyJSDiv = Sophie.createClass("my-js-div",{
 
-
       componentWillMount:function(){
-
         this.num = 0;
       },
+
+        getDefaultChildren:function(){
+          return [
+              <Div1/>,
+              <Div2/>,
+              <Div3/>
+          ]
+        },
       componentDidMount:function(){
         var num = 0;
-
-
         setInterval(()=>{
-
           this.setState({num:num++})
-
         },1000)
       },
 
@@ -75,19 +79,17 @@ var MyJSDiv = Sophie.createClass("my-js-div",{
             console.log(this.state.num)
           if(this.state.num%2 == 0){
            return   <div class="container">
-                  <Div1/>
-                  <Div2/>
-                  <Div3/>
+               {this.props.children}
               </div>
           }
           else{
 
            return    <div class="container">
                   <div>
-                      <Div1/>
-                      <Div2/>
+                      {this.props.children[0]}
+                      {this.props.children[1]}
                   </div>
-                  <Div3/>
+               {this.props.children[2]}
 
               </div>
           }
